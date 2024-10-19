eaa5d4066bf9defaf8040d952b01824067e4bb4f
Giulio Carlo D'Antonio <gcda@outlook.it>
2024-10-19 22:57:36 +0200

    Cambiato lo spazio dei nomi predefinito "WindowsApp6" -> "ShaddarNur"

--- src/My Project/Application.Designer.vb
+++ src/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.WindowsApp6.MainForm
+            Me.MainForm = Global.ShaddarNur.MainForm
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
